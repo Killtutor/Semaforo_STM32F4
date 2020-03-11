7f59141e7eca0c1a49a9de2abbebe544b69bd06b
Killtutor <agustinng14@gmail.com>
2020-03-10 21:06:47 -0400

    [add] HC-SR044 module to restart red ligth

--- src/main.c
+++ src/main.c
@@ -54,6 +54,20 @@ void SysTick_Handler(void)    //Esto me mantiene el micro corriendo al reiniciar
   HAL_SYSTICK_IRQHandler();
 }
 
+//Funcion que inicializa los pines del medidor ultrasonico
+void initUltraPins(){
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  GPIO_InitTypeDef  gpio_init_structure;
+  gpio_init_structure.Pin = GPIO_PIN_0;           //Pin 0 como entrada del echo
+  gpio_init_structure.Mode = GPIO_MODE_INPUT;
+  gpio_init_structure.Pull = GPIO_NOPULL;
+  gpio_init_structure.Speed = GPIO_SPEED_FAST;
+  HAL_GPIO_Init(GPIOB,&gpio_init_structure);
+  gpio_init_structure.Pin = GPIO_PIN_1;           //Pin 1 como salida para el hc-sr04
+  gpio_init_structure.Mode = GPIO_MODE_OUTPUT_PP;
+  HAL_GPIO_Init(GPIOB,&gpio_init_structure);
+}
+
 //Función que inicializa el/los timers
 void initializeTimers(){
   
@@ -62,7 +76,7 @@ void initializeTimers(){
   __TIM2_CLK_ENABLE();                                  //Habilito el timer2
   timerInstance.Init.Prescaler=15999;                     //Prescaler de 16000 
   timerInstance.Init.CounterMode = TIM_COUNTERMODE_UP;    //Cuento para arriba
-  timerInstance.Init.Period = 9999;                       //Periodo de 1 segundo
+  timerInstance.Init.Period = 999;                       //Periodo de 1 segundo o 9999 Probar
   timerInstance.Init.ClockDivision = 0U;
   
   HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);                  //Alta prioridad para la interrupcion el Tim2
@@ -158,7 +172,59 @@ int main (){
   initializeLeds();
   initializeTimers();
   initBoton();
-
+  initUltraPins();
+  HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_RESET);
+  int8_t chequeando = 0;
+  int16_t contador = 0;
+  int tiempo = 0;
+  int distancia = 0;
   while (1){
+
+    switch (chequeando)                     //Para ir sabiendo en que paso estoy 
+    {
+    case 0:                                 //comenzandito voy a  mandar el pulso de 10us
+      chequeando = 1;
+      HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_SET);//enciendo el pin del pulso
+      break;
+    case 1:
+      contador = contador+1;
+      if (contador==450){                               //Espero que haya pasado 10us
+        contador = 0;
+        HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_RESET); //Apago el Pin del pulso
+        chequeando = 2;
+      }
+      break;
+    case 2:
+      if (HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0) == 1){       //Espero a leer el echo cuando leo el echo acumulo los ciclos de maquina que pasa ON
+        contador = contador+1;
+      }else if (HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0) == 0 && contador>0){ //Cuando se va a OFF y si tengo algo contado
+        tiempo = contador*0.000000022;                                  //Calculo el tiempo que estuvo en ON
+        chequeando = 3;
+        contador =0;
+      }
+      if (contador >454454){                                         //No me quedare esperando para siempre si ya pasaron 10ms empiezo de nuevo
+        contador =0;
+        chequeando = 0;
+      }
+      break;
+    case 3:
+      distancia = tiempo*0.034/2;                                     //Calculo la distancia
+      if (6>distancia){                                              //si hay un peaton a 6cm del sensor reinicio rojo
+        reinicioRojo();
+      }
+      contador = contador+1;                                  
+      if (contador==454454){                                        //espero 10ms para iniciar de nuevo
+        contador =0;
+        chequeando = 0;
+      }
+      break;
+    
+    default:
+      chequeando = 1;
+      HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_SET);
+      break;
+    }
+    
+
   }
 }
